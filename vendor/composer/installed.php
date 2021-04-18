825da6a59c3ae863b70b167c45b590ce0da1b56e
vipdostyp <68096381+vipdostyp@users.noreply.github.com>
2021-04-18 19:45:55 +0600

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1fd9c327c87a2765834c0403c657bf61554eb345',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1fd9c327c87a2765834c0403c657bf61554eb345',
+    ),
+  ),
+);
